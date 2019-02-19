5b6f2b9d2d26f697ba40850ac56838f4e3836d69
沙金 <shajin3320@gmail.com>
2019-02-19 18:36:57 +0800

    提示框支持在指定页面弹出

--- View/SJAlertController.h
+++ View/SJAlertController.h
@@ -1,24 +1,32 @@
 //
-//  SJAlertController.h
-//  AutoBooking
+//  YwSyAlertView.h
+//  YwSySDK
 //
-//  Created by youwan on 2018/8/1.
-//  Copyright © 2018年 gzsm. All rights reserved.
+//  Created by youwan on 2018/6/5.
+//  Copyright © 2018年 youwan. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface SJAlertController : NSObject
+@interface SJAlertController : UIAlertController
 /** 自定义标题及提示信息的弹窗 */
 +(void)alertWithTitle:(NSString *)title message:(NSString *)message;
+/** 在指定页面弹出自定义标题及提示信息的弹窗 */
++(void)alertWithTitle:(NSString *)title message:(NSString *)message onViewController:(UIViewController *)viewController;
 /** 温馨提示 */
 +(void)hintWarmlyWithMessage:(NSString *)message;
-/** 温馨提示 */
+/** 带bolck的温馨提示 */
 +(void)hintWarmlyWithMessage:(NSString *)message block:(dispatch_block_t)block;
+/** 在指定页面弹出的温馨提示 */
++(void)hintWarmlyWithMessage:(NSString *)message onViewController:(UIViewController *)viewController;
+/** 在指定页面弹出的带block的温馨提示 */
++(void)hintWarmlyWithMessage:(NSString *)message block:(dispatch_block_t)block onViewController:(UIViewController *)viewController;
 /** 错误提示 */
 +(void)errorWithMessage:(NSString *)message;
-/** 创建弹窗 */
-+(void)alertWithTitle:(NSString *)title message:(NSString *)message leftTitle:(NSString *)leftTitle rightTitle:(NSString *)rightTitle leftBlock:(dispatch_block_t)leftBlock rightBlock:(dispatch_block_t)rightBlock;
+/** 在指定页面弹出错误提示 */
++(void)errorWithMessage:(NSString *)message onViewController:(UIViewController *)viewController;
 /** 创建弹窗 */
 +(void)makeAlertWithTitle:(NSString *)title message:(NSString *)message blockList:(NSArray *)blockList titleList:(NSArray *)titleList;
+/** 在指定页面创建弹窗创建弹窗 */
++(void)makeAlertWithTitle:(NSString *)title message:(NSString *)message blockList:(NSArray *)blockList titleList:(NSArray *)titleList onViewController:(UIViewController *)viewController;
 @end
--- View/SJAlertController.m
+++ View/SJAlertController.m
@@ -1,47 +1,63 @@
 //
-//  SJAlertController.m
-//  AutoBooking
+//  YwSyAlertView.m
+//  YwSySDK
 //
-//  Created by youwan on 2018/8/1.
-//  Copyright © 2018年 gzsm. All rights reserved.
+//  Created by youwan on 2018/6/5.
+//  Copyright © 2018年 youwan. All rights reserved.
 //
 
 #import "SJAlertController.h"
 
+@interface SJAlertController ()
+@property(nonatomic,strong)NSMutableArray *blockList;
+@end
+
 @implementation SJAlertController
 /** 自定义标题及提示信息的弹窗 */
 +(void)alertWithTitle:(NSString *)title message:(NSString *)message{
-    [self makeAlertWithTitle:title message:message blockList:nil titleList:@[@"确认"]];
+    [self makeAlertWithTitle:title message:message blockList:nil titleList:@[@"确定"]];
+}
+/** 在指定页面弹出自定义标题及提示信息的弹窗 */
++(void)alertWithTitle:(NSString *)title message:(NSString *)message onViewController:(UIViewController *)viewController{
+    [self makeAlertWithTitle:title message:message blockList:nil titleList:@[@"确定"] onViewController:viewController];
 }
 /** 温馨提示 */
 +(void)hintWarmlyWithMessage:(NSString *)message{
     [self alertWithTitle:@"温馨提示" message:message];
 }
+/** 带bolck的温馨提示 */
 +(void)hintWarmlyWithMessage:(NSString *)message block:(dispatch_block_t)block{
-    [self makeAlertWithTitle:@"温馨提示" message:message blockList:@[block] titleList:@[@"确认"]];
+    [self makeAlertWithTitle:@"温馨提示" message:message blockList:@[block] titleList:@[@"确定"]];
+}
+/** 在指定页面弹出的温馨提示 */
++(void)hintWarmlyWithMessage:(NSString *)message onViewController:(UIViewController *)viewController{
+    [self alertWithTitle:@"温馨提示" message:message onViewController:viewController];
+}
+/** 在指定页面弹出的带block的温馨提示 */
++(void)hintWarmlyWithMessage:(NSString *)message block:(dispatch_block_t)block onViewController:(UIViewController *)viewController{
+    [self makeAlertWithTitle:@"温馨提示" message:message blockList:@[block] titleList:@[@"确定"] onViewController:viewController];
 }
 /** 错误提示 */
 +(void)errorWithMessage:(NSString *)message{
     [self alertWithTitle:@"错误" message:message];
 }
-/** 创建弹窗 */
-+(void)alertWithTitle:(NSString *)title message:(NSString *)message leftTitle:(NSString *)leftTitle rightTitle:(NSString *)rightTitle leftBlock:(dispatch_block_t)leftBlock rightBlock:(dispatch_block_t)rightBlock{
-    if (![self isBlock:leftBlock]) {
-        leftBlock = ^(){};
-    }
-    if (![self isBlock:rightBlock]) {
-        rightBlock = ^(){};
-    }
-    [self makeAlertWithTitle:title message:message blockList:@[leftBlock,rightBlock] titleList:@[ifNull(leftTitle),ifNull(rightTitle)]];
+/** 在指定页面弹出错误提示 */
++(void)errorWithMessage:(NSString *)message onViewController:(UIViewController *)viewController{
+    [self alertWithTitle:@"错误" message:message onViewController:viewController];
 }
+
 /** 创建弹窗 */
 +(void)makeAlertWithTitle:(NSString *)title message:(NSString *)message blockList:(NSArray *)blockList titleList:(NSArray *)titleList{
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [self makeAlertWithTitle:title message:message blockList:blockList titleList:titleList onViewController:nil];
+}
+/** 在指定页面创建弹窗创建弹窗 */
++(void)makeAlertWithTitle:(NSString *)title message:(NSString *)message blockList:(NSArray *)blockList titleList:(NSArray *)titleList onViewController:(UIViewController *)viewController{
+    SJAlertController *alertController = [SJAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
     for (int i = 0; i < titleList.count; i++) {
         NSString *actionTitle = titleList[i];
         UIAlertAction *action = [UIAlertAction actionWithTitle:actionTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             if (blockList.count > i) {
-                if ([self isBlock:blockList[i]]) {
+                if ([self YwSy_isBlock_YwSy:blockList[i]]) {
                     dispatch_block_t block = (dispatch_block_t)blockList[i];
                     block();
                 }
@@ -49,10 +65,15 @@
         }];
         [alertController addAction:action];
     }
-    [kRootViewController presentViewController:alertController animated:YES completion:nil];
+    if ([viewController isKindOfClass:[UIViewController class]]) {
+        [viewController presentViewController:alertController animated:YES completion:nil];
+    }else{
+        [kRootViewController presentViewController:alertController animated:YES completion:nil];
+    }
 }
 /** 判断是否是block */
-+(BOOL)isBlock:(id)pBlock{
++(BOOL)YwSy_isBlock_YwSy:(id)pBlock
+{
     NSString *className = NSStringFromClass([pBlock class]);
     return  [className isEqualToString:@"__NSMallocBlock__"]||
     [className isEqualToString:@"__NSStackBlock__"]||
