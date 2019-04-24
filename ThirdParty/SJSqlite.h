a6228eabab5b2ae50ec1d3f236df88bd1310a44c
沙金 <shajin3320@gmail.com>
2019-04-24 10:00:59 +0800

    SJSqlite功能实现

--- ThirdParty/SJSqlite.h
+++ ThirdParty/SJSqlite.h
@@ -12,25 +12,29 @@
 /** 表名 */
 @property(nonatomic,strong)NSString *tableName;
 /** 主键 */
-@property(nonatomic,strong)NSString *primaryKey;
+@property(nonatomic,strong,readonly)NSString *primaryKey;
 /** 数据库文件路径 */
-@property(nonatomic,strong)NSString *dbPath;
-/** 数据库句柄 */
-@property(nonatomic,strong)FMDatabase *db;
-/** 数据库字段，key是字段名，value是字段类型 */
-@property(nonatomic,strong)NSArray *kays;
+@property(nonatomic,strong,readonly)NSString *dbPath;
+/** 创建数据库语句(主键不用写) */
+@property(nonatomic,strong,readonly)NSArray<NSString *> *createSQL;
+///** 数据库字段，key是字段名，value是字段类型 */
+//@property(nonatomic,strong,readonly)NSArray<NSString *> *keys;
 /** 插入一条数据 */
 -(BOOL)insertData:(NSDictionary *)data;
 /** 删除一条数据 */
 -(BOOL)deleteDataWhere:(NSDictionary *)data;
 /** 根据主键删除一条数据 */
--(BOOL)deleteByPrimaryKey:(NSString *)value;
+-(BOOL)deleteByPrimaryKey:(NSString *)key;
 /** 根据主键更新一条数据 */
--(BOOL)updateData:(NSDictionary *)data byPrimaryKey:(NSString *)value;
+-(BOOL)updateData:(NSDictionary *)data byPrimaryKey:(NSString *)key;
 /** 查询 */
 -(NSArray *)select;
 /** 条件查询 */
--(NSArray *)selectLimit:(NSString *)limit offset:(NSString *)offset;
+-(NSArray *)selectWhere:(NSDictionary *)where limit:(NSInteger)limit offset:(NSInteger)offset;
 /** 获取实例 */
 +(instancetype)sqlite;
+/** 判断表是否存在 */
+-(BOOL)existsTable:(NSString *)tableName;
+/** 将查询结果处理成数组形式，（由子类重写该方法） */
+-(NSArray *)executeResule:(NSArray *)result;
 @end
--- ThirdParty/SJSqlite.m
+++ ThirdParty/SJSqlite.m
@@ -8,8 +8,120 @@
 // Sqlite扩展，需要导入fmdb
 #import "SJSqlite.h"
 #import "FMDB.h"
+static id instance;
+@interface SJSqlite ()
+/** 数据库句柄 */
+@property(nonatomic,strong)FMDatabase *db;
+@end
 @implementation SJSqlite
-
+#pragma mark- CustomMethod
+/** 插入一条数据 */
+-(BOOL)insertData:(NSDictionary *)data{
+    NSMutableString *keys = [NSMutableString string];
+    NSMutableString *values = [NSMutableString string];
+    NSMutableArray *arguments = [NSMutableArray array];
+    for (NSString *key in [data allKeys]) {
+        [keys appendFormat:@"%@,",key];
+        [values appendString:@"?,"];
+        [arguments addObject:data[key]];
+    }
+    while ([keys hasSuffix:@","]) {
+        [keys deleteCharactersInRange:NSMakeRange(keys.length - 1, 1)];
+    }
+    while ([values hasSuffix:@","]) {
+        [values deleteCharactersInRange:NSMakeRange(values.length - 1, 1)];
+    }
+    return [self.db executeUpdate:[NSString stringWithFormat:@"insert into '%@' (%@) values (%@)",self.tableName,keys,values] withArgumentsInArray:arguments];
+}
+/** 删除一条数据 */
+-(BOOL)deleteDataWhere:(NSDictionary *)data{
+    NSString *whereStr = [self getWhereStringWith:data];
+    return [self executeDeleteWhere:whereStr arguments:nil];
+}
+/** 根据主键删除一条数据 */
+-(BOOL)deleteByPrimaryKey:(NSString *)key{
+    return [self executeDeleteWhere:[NSString stringWithFormat:@"%@ = ?",self.primaryKey] arguments:@[key]];
+}
+-(BOOL)executeDeleteWhere:(NSString *)where arguments:(NSArray *)arguments{
+    return [self.db executeUpdate:[NSString stringWithFormat:@"delete from '%@' where %@",self.tableName,where] withArgumentsInArray:arguments];
+}
+/** 根据主键更新一条数据 */
+-(BOOL)updateData:(NSDictionary *)data byPrimaryKey:(NSString *)key{
+    return YES;
+}
+/** 查询 */
+-(NSArray *)select{
+    return [self selectWhere:nil limit:0 offset:0];
+}
+/** 条件查询 */
+-(NSArray *)selectWhere:(NSDictionary *)where limit:(NSInteger)limit offset:(NSInteger)offset{
+    return [self executeSelect:@"*" form:self.tableName where:where limit:limit offset:offset arguments:nil];
+}
+-(NSArray *)executeSelect:(NSString *)select form:(NSString *)from where:(NSDictionary *)where limit:(NSInteger)limit offset:(NSInteger)offset arguments:(NSArray *)arguments{
+    NSMutableString *query = [NSMutableString stringWithFormat:@"select %@ from '%@' ",select ? select : @"*",from ? from : self.tableName];
+    if ([where allKeys].count > 0) {
+        [query appendString:[self getWhereStringWith:where]];
+    }
+    if (limit > 0) {
+        [query appendFormat:@" limit %ld offset %ld",limit,offset];
+    }
+    return [self executeResule:[self excuteQuery:query arguments:arguments]];
+}
+/** 根据where字典构建where条件字符串 */
+-(NSString *)getWhereStringWith:(NSDictionary *)data{
+    NSMutableArray *whereArray = [NSMutableArray array];
+    for (NSString *key in [data allKeys]) {
+        [whereArray addObject:[NSString stringWithFormat:@"%@ = %@",key,data[key]]];
+    }
+    NSString *whereStr = [whereArray componentsJoinedByString:@" and "];
+    return whereStr;
+}
+/** 建表 */
+-(void)createTable{
+    if (self.tableName.length > 0 && self.createSQL.count > 0) {
+        NSString *keys = [NSString stringWithFormat:@"'%@' integer primary key autoincrement,%@",self.primaryKey,[self.createSQL componentsJoinedByString:@","]];
+        NSString *sql = [NSString stringWithFormat:@"create table if not exists '%@' (%@)",self.tableName,keys];
+        if ([self.db executeUpdate:sql]) {
+            NSLog(@"创建数据库%@成功,path = %@",self.tableName,self.dbPath);
+        }
+    }
+}
+-(BOOL)existsTable{
+    return [self existsTable:self.tableName];
+}
+-(BOOL)existsTable:(NSString *)tableName{
+    NSString *query = [NSString stringWithFormat:@"select count(*) as 'count' from sqlite_master where type = 'table' and name = '%@'",tableName];
+    NSArray *result = [self excuteQuery:query arguments:nil];
+    if (result.count == 1) {
+        NSDictionary *dict = result[0];
+        NSLog(@"dict = %@",dict);
+        return [dict[@"count"] intValue] == 1;
+    }else{
+        return NO;
+    }
+}
+-(NSArray *)excuteQuery:(NSString *)query arguments:(NSArray *)arguments{
+    FMResultSet *result = [self.db executeQuery:query withArgumentsInArray:arguments];
+    NSMutableArray *mArray = [NSMutableArray array];
+    while ([result next]) {
+        NSMutableDictionary *mDict = [NSMutableDictionary dictionary];
+        for (int i = 0; i < result.columnCount; i++) {
+            if (![result columnIndexIsNull:i]) {
+                [mDict setObject:[result objectForColumnIndex:i] forKey:[result columnNameForIndex:i]];
+            }
+        }
+        [mArray addObject:mDict];
+    }
+    return mArray;
+}
+-(NSArray *)executeResule:(NSArray *)result{
+    return result;
+}
+#pragma mark- Setter
+-(void)setTableName:(NSString *)tableName{
+    _tableName = tableName;
+    [self createTable];
+}
 #pragma mark- Getter
 -(FMDatabase *)db{
     if (!_db) {
@@ -24,15 +136,10 @@
 #pragma mark- 初始化
 -(instancetype)init{
     if (self = [super init]) {
-        NSString *keys = [self.kays componentsJoinedByString:@","];
-        NSString *sql = [NSString stringWithFormat:@"create table if not exists '%@' (%@)",self.tableName,keys];
-        if (![self.db executeUpdate:sql]) {
-            NSLog(@"数据表创建失败");
-        }
+        [self createTable];
     }
     return self;
 }
-/** 获取实例 */
 +(instancetype)sqlite{
     return [self new];
 }
