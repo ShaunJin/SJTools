9588adfafc841855063edb40305d64b65afe4f24
沙金 <shajin3320@qq.com>
2022-03-03 15:00:03 +0800

    SJFileManager和SJTableView修改

--- Category/UITableView+SJTableView.m
+++ Category/UITableView+SJTableView.m
@@ -7,12 +7,25 @@
 //
 
 #import "UITableView+SJTableView.h"
-
+#import <objc/runtime.h>
 @implementation UITableView (SJTableView)
++(void)load{
+    Method exchangeMethod = class_getInstanceMethod(self, @selector(initWithFrame:style:));
+    Method customMethod = class_getInstanceMethod(self, @selector(customInitWithFrame:style:));
+    method_exchangeImplementations(exchangeMethod, customMethod);
+}
 /** 批量注册CellClass{identifier:cellClassName} */
 -(void)registerClass:(NSDictionary *)dict{
     for (NSString *key in [dict allKeys]) {
         [self registerClass:NSClassFromString(dict[key]) forCellReuseIdentifier:key];
     }
 }
+-(instancetype)customInitWithFrame:(CGRect)frame style:(UITableViewStyle)style{
+    [self customInitWithFrame:frame style:style];
+    if (@available(iOS 15.0, *)) {
+        self.sectionHeaderTopPadding = 0.0f;
+    }
+    return self;
+}
+
 @end
