b9d08db60eabc2c01b133ce7be7655ce35e2b322
沙金 <shajin3320@qq.com>
2022-03-24 16:21:13 +0800

    UITableViewCell+SJTableViewCell

---
+++ Category/UITableViewCell+SJTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell+SJTableViewCell.h
+//  emerg
+//
+//  Created by Air on 2022/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITableViewCell (SJTableViewCell)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Category/UITableViewCell+SJTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  UITableViewCell+SJTableViewCell.m
+//  emerg
+//
+//  Created by Air on 2022/3/24.
+//
+
+#import "UITableViewCell+SJTableViewCell.h"
+
+@implementation UITableViewCell (SJTableViewCell)
++(void)load{
+    Method exchangeMethod = class_getInstanceMethod(self, @selector(initWithStyle:reuseIdentifier:));
+    Method customMethod = class_getInstanceMethod(self, @selector(customInitWithStyle:reuseIdentifier:));
+    method_exchangeImplementations(exchangeMethod, customMethod);
+}
+-(instancetype)customInitWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    [self customInitWithStyle:style reuseIdentifier:reuseIdentifier];
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
+    return self;
+}
+@end
